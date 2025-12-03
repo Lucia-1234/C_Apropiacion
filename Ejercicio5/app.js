d12cef9e4f0c403054f529d5b496729ca043f4c2
Lucia <lulizcano.aa@hotmail.com>
2025-12-03 16:13:02 -0500

    Ejercicio 5 finalizado haciendo uso de las condicionales

---
+++ Ejercicio5/app.js
@@ -0,0 +1,59 @@
+// meses del año
+import prompt_sync from "prompt-sync";
+const prompt = prompt_sync();
+
+//se solicita el numero de mes al usuario
+let mes= parseInt(prompt("Ingrese el número del mes: "));
+
+//inicia condicional que muestra el nombre del mes segun el numero
+
+if (mes==1){
+  console.log("Enero")
+};
+
+if (mes==2){
+  console.log("Febrero")
+};
+
+if (mes==3){
+  console.log("Marzo")
+};
+
+if (mes==4){
+  console.log("Abril")
+};
+
+if (mes==5){
+  console.log("Mayo")
+};
+
+if (mes==6){
+  console.log("Junio")
+};
+
+if (mes==7){
+  console.log("Julio")
+};
+
+if (mes==8){
+  console.log("Agosto")
+};
+
+if (mes==9){
+  console.log("Septiembre")
+};
+
+if (mes==10){
+  console.log("Octubre")
+};
+
+if (mes==11){
+  console.log("Noviembre")
+};
+
+if (mes==12){
+  console.log("Diciembre")
+}
+
+
+
